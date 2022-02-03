3c5a0380e0bd75bfb52badd411e05f965ea4c05b
Laleh <92782353+Laleh4@users.noreply.github.com>
2022-02-03 11:08:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app=FastAPI()
 
 
 
-
+"""
 f=open('data_Milestone3.pkl', 'rb')
 val_final=pickle.load(f)
 seuil_final=pickle.load(f)
@@ -27,7 +27,7 @@ rf_grid=pickle.load(f)
 rf_model = rf_grid.best_estimator_
 f.close()
 
-"""
+
 X_valids=pd.read_csv("X_valid.csv")
 X_valids=X_valids.iloc[:,1:]
 """
