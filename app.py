a4a4fbe5bd0aab1815bf2092d7aa19dda679cb28
Laleh <92782353+Laleh4@users.noreply.github.com>
2022-02-03 15:53:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ Created on Mon Jan 31 09:43:41 2022
 from fastapi import FastAPI
 import uvicorn
 import pickle
-#from BankNotes import BankNote
+
 from Clients import Client
 import pandas
-
 
 
 app=FastAPI()
@@ -38,27 +37,7 @@ def greet():
 
 
 
-@app.post("/predict")
-def predictx(req: BankNote):
-    
 
-    preg=req.pregnacies
-    glucose=req.glucose
-    bp=req.bp
-    skinthickness=req.skinthickness
-    insulin=req.insulin
-    bmi=req.bmi
-    dpf=req.dpf
-    age=req.age
-    features=list([preg,glucose,bp,skinthickness, insulin,bmi,dpf,age])
-    
-    predict=model.predict([features])
-    probab=model.predict_proba([features])
-    if (predict==1):
-        return {"ans":"You have benn tested positive with {} probability".format(probab[0][1])}
-    else:
-        return {"ans":"You have benn tested negative with {} probability".format(probab[0][0])}
-    
     
 
     
